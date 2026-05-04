c43433ecca6d03f4f3036fc7556145c7330244c2
AyparaKerimova <eelify1@gmail.com>
2026-05-04 15:50:37 +0400

    backend deploy add

--- frontend/src/app/baseApi.js
+++ frontend/src/app/baseApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const baseApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:3001/api',
+    baseUrl: 'https://filmbox-a22g.onrender.com/api',
     prepareHeaders: (headers, { getState }) => {
       const token = getState().auth.token;
       if (token) headers.set('authorization', `Bearer ${token}`);
